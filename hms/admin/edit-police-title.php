716eb614b2781b8fcae2868f26e96c9c08f87c39
pachekox <www.pacheko.inc@gmail.com>
2022-06-29 22:06:04 -0700

    update edit-police.php

--- hms/admin/edit-police-title.php
+++ hms/admin/edit-police-title.php
@@ -7,16 +7,16 @@ check_login();
 $id=intval($_GET['id']);// get value
 if(isset($_POST['submit']))
 {
-$docspecialization=$_POST['doctorspecilization'];
-$sql=mysqli_query($con,"update  doctorSpecilization set specilization='$docspecialization' where id='$id'");
-$_SESSION['msg']="Doctor Specialization updated successfully !!";
+$docspecialization=$_POST['policespecilization'];
+$sql=mysqli_query($con,"update  policeSpecilization set specilization='$docspecialization' where id='$id'");
+$_SESSION['msg']="police Specialization updated successfully !!";
 } 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Admin | Edit Doctor Specialization</title>
+		<title>Admin | Edit police Specialization</title>
 		
 		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
@@ -47,14 +47,14 @@ $_SESSION['msg']="Doctor Specialization updated successfully !!";
 						<section id="page-title">
 							<div class="row">
 								<div class="col-sm-8">
-									<h1 class="mainTitle">Admin | Edit Doctor Specialization</h1>
+									<h1 class="mainTitle">Admin | Edit police Specialization</h1>
 																	</div>
 								<ol class="breadcrumb">
 									<li>
 										<span>Admin</span>
 									</li>
 									<li class="active">
-										<span>Edit Doctor Specialization</span>
+										<span>Edit police Specialization</span>
 									</li>
 								</ol>
 							</div>
@@ -69,7 +69,7 @@ $_SESSION['msg']="Doctor Specialization updated successfully !!";
 										<div class="col-lg-6 col-md-12">
 											<div class="panel panel-white">
 												<div class="panel-heading">
-													<h5 class="panel-title">Edit Doctor Specialization</h5>
+													<h5 class="panel-title">Edit police Specialization</h5>
 												</div>
 												<div class="panel-body">
 								<p style="color:red;"><?php echo htmlentities($_SESSION['msg']);?>
@@ -77,16 +77,16 @@ $_SESSION['msg']="Doctor Specialization updated successfully !!";
 													<form role="form" name="dcotorspcl" method="post" >
 														<div class="form-group">
 															<label for="exampleInputEmail1">
-																Edit Doctor Specialization
+																Edit police Specialization
 															</label>
 
 	<?php 
 
 $id=intval($_GET['id']);
-	$sql=mysqli_query($con,"select * from doctorSpecilization where id='$id'");
+	$sql=mysqli_query($con,"select * from policeSpecilization where id='$id'");
 while($row=mysqli_fetch_array($sql))
 {														
-	?>		<input type="text" name="doctorspecilization" class="form-control" value="<?php echo $row['specilization'];?>" >
+	?>		<input type="text" name="policespecilization" class="form-control" value="<?php echo $row['specilization'];?>" >
 	<?php } ?>
 														</div>
 												
