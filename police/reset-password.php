37252e2d6a1b42bb53d96499b09623ebe7177611
pachekox <www.packeo.inc@gmail.com>
2022-07-15 18:05:18 +0300

    resolve naming in police/reset-password.php

--- police/reset-password.php
+++ police/reset-password.php
@@ -53,14 +53,14 @@ return true;
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
-				<a href="../index.html"><h2> HMS | Patient Reset Password</h2></a>
+				<a href="../index.html"><h2> CRTS | police Reset Password</h2></a>
 				</div>
 
 				<div class="box-login">
 					<form class="form-login" name="passwordreset" method="post" onSubmit="return valid();">
 						<fieldset>
 							<legend>
-								Patient Reset Password
+								police Reset Password
 							</legend>
 							<p>
 								Please set your new password.<br />
@@ -97,7 +97,7 @@ return true;
 					</form>
 
 					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> CRTS</span>. <span>All rights reserved</span>
 					</div>
 			
 				</div>
